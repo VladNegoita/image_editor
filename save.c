492c50303b18ceab4a6b2565d4554ebccde086d3
VladNegoita <vlad1negoita@gmail.com>
2022-01-06 00:07:24 +0200

    Improvment

--- save.c
+++ save.c
@@ -27,14 +27,15 @@ void save_image(char *instruction, image *current_image)
 		return;
 	}
 
-	int max_value = MAX_VALUE;////////////////////////////////////////////////
+	int max_value = MAX_VALUE;///////////////////////////////////////////////
 
 	if (strstr(instruction, "ascii")) {
 		if (current_image->type == 2 || current_image->type == 5)
 			fprintf(out, "P%d\n", 2);
 		else
 			fprintf(out, "P%d\n", 3);
-		fprintf(out, "%d %d\n", current_image->mat.height, current_image->mat.width);
+		fprintf(out, "%d ", current_image->mat.height);
+		fprintf(out, "%d\n", current_image->mat.width);
 		fprintf(out, "%d\n", max_value);
 		for (int h = 0; h < current_image->mat.height; ++h) {
 			for (int w = 0; w < current_image->mat.width; ++w) {
@@ -53,7 +54,8 @@ void save_image(char *instruction, image *current_image)
 			fprintf(out, "P%d\n", 5);
 		else
 			fprintf(out, "P%d\n", 6);
-		fprintf(out, "%d %d\n", current_image->mat.height, current_image->mat.width);
+		fprintf(out, "%d ", current_image->mat.height);
+		fprintf(out, "%d\n", current_image->mat.width);
 		fprintf(out, "%d\n", max_value);
 		fclose(out);
 		out = fopen(file_name, "ab");
